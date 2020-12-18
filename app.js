fba2181c97f49845f63611d409b8dc57c86c2b05
David Rodriguez <davidrodriguez@BOMC9081907.local>
2020-12-18 00:33:10 -0500

    Initial commit Circle CI study

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var exports = module.exports = {};
+
+function welcomeMessage() {
+    var message = "Welcome to CI/CD 101 using CircleCI!";
+    return message;
+}
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render("index", { message: welcomeMessage() });
+});
+
+var server = app.listen(5000, function() {
+    console.log("Node server is running..");
+});
+
+module.exports = server;
+module.exports.welcomeMessage = welcomeMessage;
